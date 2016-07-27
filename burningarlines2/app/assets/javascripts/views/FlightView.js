4318f31d0f1f9b9d2e2f0d2aea5df854d4b170b7
Phani Ganti <phaniganti@192-168-1-105.tpgi.com.au>
2016-07-27 22:14:01 +1000

    Added the functionality in FlightView to display the plane number as link

--- burningarlines2/app/assets/javascripts/views/FlightView.js
+++ burningarlines2/app/assets/javascripts/views/FlightView.js
@@ -19,11 +19,18 @@ app.FlightView = Backbone.View.extend({
       var origin = $("<td>");
       var destination = $("<td>");
       var date = $("<td>");
-          date.text(r.get('date'));
-          number.text(r.get('number'));
-          origin.text(r.get('origin'));
-          destination.text(r.get('destination'));
-          $("#flightTableBody").append(number).append(origin).append(destination).append(date).append('<tr>');
+      date.text(r.get('date'));
+
+      var url = "http://localhost:3000/planes/"+r.get('id');
+
+      var aTag = $('<a>', {href: url });
+      aTag.text(r.get('number'));// Populate the text with what's already there
+      number.text('').append(aTag);
+
+      origin.text(r.get('origin'));
+      destination.text(r.get('destination'));
+      $("#flightTableBody").append(number).append(origin).append(destination).append(date).append('<tr>');//
+      
       });
       // this.$el.html(flightViewTemplater( r));
 
