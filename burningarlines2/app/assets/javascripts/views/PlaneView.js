82ee28572adaa73d07317e0740f50308b7ce5e22
Phani Ganti <phaniganti@Phanis-MacBook-Pro.local>
2016-07-28 11:57:44 +1000

    Changed the plane view to get the rows and columns

--- burningarlines2/app/assets/javascripts/views/PlaneView.js
+++ burningarlines2/app/assets/javascripts/views/PlaneView.js
@@ -3,7 +3,17 @@ var app = app || {};
 app.PlaneView = Backbone.View.extend({
   el: "#planeInfo",
 
-  render: function(){
-    console.log("I m here");
+  render: function(planeId){
+// debugger;
+    var planeViewTemplate = $("#planeInfo").html();
+    var planeViewTemplater = _.template(planeViewTemplate);
+
+    var plane = app.showplanes.findWhere({id: parseInt(planeId)});
+    var rows = plane.get("row");
+    console.log(rows);
+    var columns = plane.get("column");
+    console.log(columns);
+    
+
   }
 });
